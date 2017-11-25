3e0bb3d568403ebd26ec1a6399beaea2fc616cde
Syed Isam Hashmi <prosdkr@me.com>
2017-11-24 23:08:55 -0600

    Create main.cpp

---
+++ rgbaconvertertool/main.cpp
@@ -0,0 +1,48 @@
+//
+//  main.cpp
+//  rgbato16bytestrings
+//
+//  Created by Isam Hashmi on 11/24/17.
+//  Copyright © 2017 Isam Hashmi. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main(int argc, const char * argv[])
+{
+    std::string filename = ""; //Input a file name to be converted to 16byte long string seperated by '"' and '\n'
+    
+    std::ifstream is (filename, std::ifstream::binary );
+    
+    if (is) {
+        is.seekg (0, is.end);
+        long long int length = is.tellg();
+        is.seekg (0, is.beg);
+        
+        char * buffer = new char [length];
+        
+        // read data as a block:
+        is.read (buffer,length);
+        
+       
+        is.close();
+        
+        
+        for(int x = 0; x < length; x++)
+        {
+            if(x %16 == 0)
+            {
+                cout << "\"" << endl << "\"" ;
+            }
+            printf("\\x%2x", (unsigned char)buffer[x]);
+        
+            
+        }
+        
+        
+        delete[] buffer;
+    }
+    cout << endl << endl;
+    return 0;
+}
