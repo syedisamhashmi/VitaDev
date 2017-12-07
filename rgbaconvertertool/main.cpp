5ebff82edd60027aa9bb233aaa4b190aa0f52f27
Isam Hashmi <isamhashmi@prosdkr.local>
2017-12-06 18:55:46 -0600

    Full print fix

--- rgbaconvertertool/main.cpp
+++ rgbaconvertertool/main.cpp
@@ -12,35 +12,37 @@ using namespace std;
 int main(int argc, const char * argv[])
 {
     std::string filename = ""; //Input a file name to be converted to 16byte long string seperated by '"' and '\n'
-    
+
     std::ifstream is (filename, std::ifstream::binary );
-    
+
     if (is) {
         is.seekg (0, is.end);
         long long int length = is.tellg();
         is.seekg (0, is.beg);
-        
+
         char * buffer = new char [length];
-        
+
         // read data as a block:
         is.read (buffer,length);
-        
-       
+
+
         is.close();
-        
-        
+
+
         for(int x = 0; x < length; x++)
         {
-            if(x %16 == 0)
+            if(x== 0 )
+                cout << "\"";
+            if(x != 0 && x %16 == 0)
             {
                 cout << "\"" << endl << "\"" ;
             }
             printf("\\x%02x", (unsigned char)buffer[x]);
-        
-            
+
+
         }
-        
-        
+        cout << "\"";
+
         delete[] buffer;
     }
     cout << endl << endl;
